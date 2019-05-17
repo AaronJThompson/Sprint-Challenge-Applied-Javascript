c5a0520ad549c6f95b5e37076e394a29981566c7
AaronJThompson <aaronjthompson98@gmail.com>
2019-05-17 10:43:22 +0100

    Self-Stretch: Start trying to animate card movement

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -39,7 +39,9 @@ class TabLink {
 
     // Select all of the elements with the .card class on them
     const cards = document.querySelectorAll(".cards-container .card");
-
+    this.cards.forEach((card) => {
+      card.recordPos();
+    })
     // Iterate through the NodeList setting the display style each one to 'none'
     cards.forEach(card => {
       card.style.display = 'none';
@@ -47,7 +49,7 @@ class TabLink {
     
     // Add a class of ".active-tab" to this.tabElement
     this.tabElement.classList.add("active-tab");
-  
+    
     // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class. Just un-comment the code and study what is happening here.
     this.cards.forEach(card => card.selectCard());
   }
@@ -58,9 +60,22 @@ class TabCard {
     // Assign this.cardElement to the cardElement DOM reference
     this.cardElement = cardElement;
   }
+  recordPos(){
+    this.x = this.cardElement.offsetLeft;
+    this.y = this.cardElement.offsetTop;
+  }
   selectCard(){
     // Update the style of this.cardElement to display = "flex"
     this.cardElement.style.display = "flex";
+    let x = this.cardElement.transform.x + this.x - this.cardElement.x;
+    let y = this.cardElement.transform.y + this.y - this.cardElement.y;
+    
+    // Tween to 0,0 to remove the transforms
+    TweenMax.fromTo(this.cardElement, duration, { x: x, y: y }, { x: 0, y: 0, ease: Power1.easeInOut }); 
+  }
+
+  animateMove(oldX, oldY, newX, newY) {
+
   }
 
 }
